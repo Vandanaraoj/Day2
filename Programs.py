862751000847fcc715555209dcfdf206002ec495
Vandanaraoj <vandanarao36@gmail.com>
2026-07-19 14:20:13 +0530

    Programs

---
+++ Programs.py
@@ -0,0 +1,4 @@
+Num1 =int(input("Enter first number: "))
+Num2 =int(input("Enter second number: "))
+LargestNumber = max(Num1, Num2)  # This will compare the numbers as integers.
+print("The larger number is: ", LargestNumber)
